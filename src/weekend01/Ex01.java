ee8e6feae0f26c3a2e7cba96bbc764758ecf1a8b
dewbuster <chwon301@gmail.com>
2024-07-07 21:49:37 +0900

    weekend01-02

--- src/weekend01/Ex01.java
+++ src/weekend01/Ex01.java
@@ -7,21 +7,32 @@ public class Ex01 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("한 문자(ch) 입력 ? ");
-		char ch = scanner.next().charAt(0);
+		char ch;
+		boolean flag = false;
 		
-		if ( 'A' <= ch && ch <= 'Z' ) {
-			System.out.println("대문자입니다.");
-		} else if ( 'a' <= ch && ch <= 'z' ) {
-			System.out.println("소문자입니다.");
-		} else if ( '0' <= ch && ch <= '9' ) {
-			System.out.println("숫자입니다.");
-		} else if ( ('#' <= ch && ch <= '&') || ch == '!' || ch == '@' ) {
-			System.out.println("특수문자입니다.");
-		}
-		
+		do {
+			System.out.print("한 문자(ch) 입력 ? ");
+			ch = scanner.next().charAt(0);
+			
+			if ( 'A' <= ch && ch <= 'Z' ) {
+				System.out.println("대문자입니다.");
+				flag = false;
+			} else if ( 'a' <= ch && ch <= 'z' ) {
+				System.out.println("소문자입니다.");
+				flag = false;
+			} else if ( '0' <= ch && ch <= '9' ) {
+				System.out.println("숫자입니다.");
+				flag = false;
+			} else if ( ('#' <= ch && ch <= '&') || ch == '!' || ch == '@' ) {
+				System.out.println("특수문자입니다.");
+				flag = false;
+			} else {
+				System.out.print("다시 ");
+				flag = true;
+			}
+		} while (flag);
+
 		scanner.close();
-		
 	}
 
 }
